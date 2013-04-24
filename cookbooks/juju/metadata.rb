94f8c494c367f3969d72062c6477250d72b61c7a
Richard Shade <rshade@rightscale.com>
2013-04-24 13:39:52 -0500

    first pass at juju

---
+++ cookbooks/juju/metadata.rb
@@ -0,0 +1,22 @@
+name             'juju'
+maintainer       'RightScale Inc'
+maintainer_email 'premium@rightscale.com'
+license          'Apache 2.0'
+description      'Installs/Configures juju'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+
+depends "rightscale"
+depends "apt"
+
+recipe "juju::default", "installs and configures juju"
+
+attribute "juju/access_key_id", 
+  :display_name => "Access Key ID",
+  :description => "access_key_id"
+
+attribute "juju/secret_access_key",
+  :display_name => "Secret Access Key",
+  :description => "Secret Access Key"
+
